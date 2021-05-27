dc0b1887fc12242b6ceb0458f5264a54c31c4c39
swaggp <mutebinuhu1@gmail.com>
2021-05-27 00:43:32 -0700

    add the front end

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ app.use('/api/profile', require('./Routes/api/profile'));
 app.use('/api/auth', require('./Routes/api/auth'));
 
 
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
    console.log(`App runs on port ${PORT}`)
