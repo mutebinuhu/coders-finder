cfaf6d9b48563f9e1ce9a1f5aef719f181ad8e28
swaggp <mutebinuhu1@gmail.com>
2021-05-27 02:49:25 -0700

    adding the react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
-import {Fragment} from 'react'
+import {Fragment} from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register'
 
 
 const App = () => {
   return(
-  <Fragment>
-    <Navbar/>
-    <Landing />
+  <Router>
+    <Fragment>
+      <Navbar/>
+      <Route exact path ='/' component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path ='/login' component={Login} />
+          <Route exact path ='/register' component={Register} />
+
+        </Switch>
+      </section>
   </Fragment>
+  </Router>
   )
  
 }
