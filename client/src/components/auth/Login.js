6bd7844b332ab8f5b6d9ef5033b68e879ed9acdd
swaggp <mutebinuhu1@gmail.com>
2021-06-02 23:53:03 -0700

    implementing the register and login apis

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,19 +1,50 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios'
 
 const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    })
+    const {email, password} = formData;
+    const handleChange = e =>{
+        setFormData({...formData, [e.target.name]: e.target.value})
+    }
+    const handleSubmit = async e =>{
+        e.preventDefault()
+        console.log(formData)
+        try {
+            const loginData = {
+                email,
+                password
+            }
+            const config = {
+                headers:{
+                    'Content-Type': 'Application/json'
+                }
+            }
+            const body = JSON.stringify(loginData)
+            const res = await axios.post('/api/auth', body, config)
+            console.log(res.data)
+        } catch (err) {
+            console.error(err.response)
+        }
+    }
     return (
         <Fragment> 
             <section className="container">
                 <h1 className="large text-primary">Sign In</h1>
                 <p className="lead"><i className="fas fa-user"></i> Sign into Your Account</p>
-                <form className="form" action="dashboard.html">
+                <form className="form" onSubmit={e=>handleSubmit(e)}>
                     <div className="form-group">
                     <input
                         type="email"
                         placeholder="Email Address"
                         name="email"
                         required
+                        value = {email}
+                        onChange={e=>handleChange(e)}
                     />
                     </div>
                     <div className="form-group">
@@ -21,6 +52,8 @@ const Login = () => {
                         type="password"
                         placeholder="Password"
                         name="password"
+                        value={password}
+                        onChange={e=>handleChange(e)}
                     />
                     </div>
                     <input type="submit" className="btn btn-primary" value="Login" />
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,18 +1,54 @@
-import React, {Fragment} from 'react'
-import {Link} from 'react-router-dom'
+import React, {Fragment, useState} from 'react'
+import {Link} from 'react-router-dom';
+import axios from 'axios'
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+  const {name, email, password,password2 } = formData;
+  const onChange =(e)=>{
+    setFormData({
+      ...formData, [e.target.name]:e.target.value
+    })
+  }
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+   if(password !== password2){
+     console.log('passwords dont much')
+   }else{
+   try {
+    const newUser={
+      name,email, password
+    }
+    const config = {
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    }
+    const body = JSON.stringify(newUser)
+    const res = await axios.post('/api/users', body, config)
+    console.log(res.data.token)
+   } catch (err) {
+     console.error(err.response)
+   }
+   }
+   
+  }
     return (
         <Fragment>
            <section className="container">
       <h1 className="large text-primary">Sign Up</h1>
       <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
-      <form className="form" action="create-profile.html">
+      <form className="form" onSubmit={e=>handleSubmit(e)}>
         <div className="form-group">
-          <input type="text" placeholder="Name" name="name" required />
+          <input type="text" placeholder="Name" name="name" value={name} onChange={e=>onChange(e)} required/>
         </div>
         <div className="form-group">
-          <input type="email" placeholder="Email Address" name="email" />
+          <input type="email" placeholder="Email Address" name="email" value={email} onChange={e=>onChange(e)} required />
           <small className="form-text"
             >This site uses Gravatar so if you want a profile image, use a
             Gravatar email</small
@@ -24,6 +60,7 @@ const Register = () => {
             placeholder="Password"
             name="password"
             minLength="6"
+            value={password} onChange={e=>onChange(e)}
           />
         </div>
         <div className="form-group">
@@ -32,6 +69,8 @@ const Register = () => {
             placeholder="Confirm Password"
             name="password2"
             minLength="6"
+            value={password2} onChange={e=>onChange(e)}
+
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Register" />
